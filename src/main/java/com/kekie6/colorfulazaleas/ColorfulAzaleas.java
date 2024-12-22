1550412d8964f10ad07a73db7a71cb0120cf1a13
DaFuqs <dominik.m.schmidt@googlemail.com>
2024-12-22 10:34:25 +0100

    fin!~

--- src/main/java/com/kekie6/colorfulazaleas/ColorfulAzaleas.java
+++ src/main/java/com/kekie6/colorfulazaleas/ColorfulAzaleas.java
@@ -3,6 +3,7 @@ package com.kekie6.colorfulazaleas;
 import com.kekie6.colorfulazaleas.decorators.*;
 import com.kekie6.colorfulazaleas.registry.*;
 import net.fabricmc.api.*;
+import net.fabricmc.fabric.api.tag.convention.v2.*;
 import net.minecraft.core.*;
 import net.minecraft.core.registries.*;
 import net.minecraft.resources.*;
@@ -11,8 +12,7 @@ import org.slf4j.*;
 
 public class ColorfulAzaleas implements ModInitializer {
     public static final String MOD_ID = "colorfulazaleas";
-    public static final String MOD_NAME = "Colorful Azaleas";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     public static final TreeDecoratorType<ColorfulTreeDecorator> COLORFUL_TREE_DECORATOR = Registry.register(BuiltInRegistries.TREE_DECORATOR_TYPE, id("colorful_tree_decorator"), new TreeDecoratorType<>(ColorfulTreeDecorator.CODEC));
 
